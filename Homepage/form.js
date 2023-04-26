e93454db1db8d2976205bc1ca4b62a37d742852d
AquaMarine <up1057736@upnet.gr>
2023-04-27 00:03:50 +0300

    forms added

--- Homepage/form.js
+++ Homepage/form.js
@@ -1,21 +1,40 @@
 
-        let signup = document.querySelector(".signup-form");
-        let sign = document.querySelector(".sign");
-        sign.addEventListener("click", ()=> {
-            signup.style.display = "block";
-        })
-        sign.onclick = function() {
-            document.querySelector("body").style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+let signup = document.querySelector(".signup-form");
+let sign = document.querySelector(".sign");
+
+//for the sign in button
+sign.addEventListener("click", ()=> {
+    signup.style.display = "block";
+})
+sign.onclick = function() {
+    const elements = document.querySelectorAll("*")
+    elements.forEach((element) => {
+        if(element.tagName.toLowerCase() !== "form"){
+            element.style.opacity = 0.2;
+        }   
+    });
+            
+    console.log("clicked")
+}
+
+
+// for the login button
+
+
+let login = document.querySelector(".login-form");
+let log = document.querySelector(".log");
+log.addEventListener("click", ()=> {
+    login.style.display = "block";
+})
+        log.onclick = function() {
+            const elements = document.querySelectorAll("*")
+            elements.forEach((element) => {
+                if(element.tagName.toLowerCase() !== "form"){
+                    //element.style.opacity = 0.8;
+                }   
+            });
+            
             console.log("clicked")
         }
 
-        let login = document.querySelector(".login-form");
-        let log = document.querySelector(".log");
-        log.addEventListener("click", ()=> {
-            login.style.display = "block";
-        })
-        log.onclick = function() {
-            document.querySelector("body").style.backgroundColor = "rgba(0, 0, 0, 0.9)";
-            console.log("clicked")
-        }
     
