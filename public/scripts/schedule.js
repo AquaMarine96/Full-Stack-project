4f5bf1b471675ddfe8cddf66a622e7ec65ef35b4
AquaMarine <up1057736@upnet.gr>
2023-05-28 19:19:30 +0300

    commits

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -1,36 +1,67 @@
-console.log("reloaded");
-
-document.addEventListener('DOMContentLoaded', function () {
-    let courseForm = document.querySelector('#selection');
-    let saveButton = document.querySelector('#save');
-    let selectedCoursesList = document.querySelector('#courseList');
-    let declareButton = document.querySelector('#declare');
-    let changeButton = document.querySelector('#change');
+console.log("lol");
 
 
-    declareButton.addEventListener('click', function () {
-        courseForm.style.display = "block";
-        console.log("clicked");
-    });
+let saveButton = document.querySelector('#save');
 
-    saveButton.addEventListener('click', function () {
-        // Get all selected courses
-        let selectedCourses = courseForm.querySelectorAll('input[type="checkbox"]:checked');
+let declareButton = document.querySelector('#declare');
+let changeButton = document.querySelector('#change');
 
-        // Clear existing list items
-        selectedCoursesList.innerHTML = '';
-
-        // Create list items for selected courses
-        for (let i = 0; i < selectedCourses.length; i++) {
-            let course = selectedCourses[i];
-            let listItem = document.createElement('li');
-            listItem.textContent = course.value;
-            selectedCoursesList.appendChild(listItem);
-        }
-        selectedCoursesList.style.display = "block";
-        
-        changeButton.getElementsByClassName.display = 'block';
-        courseForm.getElementsByClassName.display = "none";
-        console.log("saved");
-    });
+declareButton.addEventListener('click', function () {
+    form.style.display = "block";
+    console.log("clicked");
 });
+    
+
+// Get all selected courses
+const form = document.getElementById('selection');
+const lista = document.querySelector("#courseList");
+const coursesDiv = document.querySelector(".selectedCourses");
+
+// Function to handle checkbox changes
+function handleCheckboxChange(event) {
+  if (event.target.checked) {
+    // Create a new <li> element with the course content
+    const li = document.createElement('li');
+    li.textContent = event.target.nextSibling.textContent;
+    
+    // Add the <li> element to the <ul> element
+    lista.appendChild(li);
+
+  }else{
+    lista.removeChild(lista.lastChild);
+  }
+}
+
+// Add event listeners to the checkboxes
+const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', handleCheckboxChange);
+});
+
+// Create list items for selected courses
+
+saveButton.addEventListener('click', function (event) {
+    form.style.display = "none";
+    console.log("saved");
+    changeButton.style.display = "block";
+    declareButton.style.display = "none";
+    event.preventDefault();
+    coursesDiv.style.display = "block";
+});
+
+changeButton.addEventListener('click', function () {
+    form.style.display = "block";
+    console.log("changed");
+    changeButton.style.display = "none";
+    lista.style.display = "none";
+});
+
+if(isEmpty("#courseList")){
+    coursesDiv.style.display = "none";
+    declareButton.style.display = "block";
+    changeButton.style.display = "none";    
+}
+
+function isEmpty(id) {
+    return document.getElementById(id).innerHTML.trim() == ""
+  }
